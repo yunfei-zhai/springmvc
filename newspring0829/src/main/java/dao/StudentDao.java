4b461a45a5d5cdff46d44f898761f9c6ee17d84e
Administrator <Administrator@192.168.110.226>
2019-08-30 08:38:48 +0800

    提交

--- newspring0829/src/main/java/dao/StudentDao.java
+++ newspring0829/src/main/java/dao/StudentDao.java
@@ -10,6 +10,6 @@ import model.Student;
 
 @Repository
 public interface StudentDao {
-@Select("select student.*,class.classname classname from student inner join class on student.classid=class.classid ${txt}" )
+@Select("select student.*,class.classname from student inner join class on student.classid=class.classid ${txt}" )
 public List<Student> select(@Param("txt")String txt);
 }
